480361254a17dc08e2e53992469b30ebe7b5c545
Terence21 <terencepeterson21@gmail.com>
2020-12-10 10:57:47 -0800

    created skeleton hello world android app

---
+++ app/src/main/java/temple/edu/zomato_randomizer/MainActivity.java
@@ -0,0 +1,21 @@
+package temple.edu.zomato_randomizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isSavedLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isSavedLogin){
+            //  auto load options activity
+        }
+
+
+    }
+}
